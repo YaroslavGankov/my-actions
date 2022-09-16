e17812b2a880fe9767ce068ccd3fd78952aa6156
Yaroslav Gankov <yaroslav.gankov@ardas.biz>
2022-09-16 15:50:39 +0300

    test actions/cache

---
+++ tf-origins/docker-start.tf
@@ -0,0 +1,27 @@
+#file for test github action from cache-test.yml
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "null_resource" "command1" {
+    provisioner "local-exec" {
+        command = "docker ps"
+    }
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
+
+resource "null_resource" "command1" {
+    provisioner "local-exec" {
+        command = "echo Ololo $(date)"
+    }
+}
---
+++ tf-origins/providers.tf
@@ -0,0 +1,12 @@
+#if this file was changed then 'terraform init' will execute
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
