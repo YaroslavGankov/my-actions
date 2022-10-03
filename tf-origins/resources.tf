ce600c390676d8342047e2d99d2debae86384e12
Yaroslav Gankov <yaroslav.gankov@transportexchangegroup.com>
2022-10-03 16:43:52 +0300

    test terraform without localstack

---
+++ tf-origins/resources.tf
@@ -0,0 +1,20 @@
+variable vpc_cidr_mask16 {
+    description = "First two numbers of the VPC (without dot in the end)"
+    type = string
+    default = "10.173"
+}
+
+locals {
+  vpc_cidr = "${var.vpc_cidr_mask16}.0.0/16"
+}
+
+resource "aws_vpc" "skiff_vpc" {
+    cidr_block = local.vpc_cidr
+    tags = {
+        Name = "${var.vpc_cidr_mask16}.x.x-skiff-delete"
+    }
+}
+
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "delete-test-skiff-bucket"
+}
