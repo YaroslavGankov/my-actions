a6dfe63a389a36e3b6168c05248c1d6e9d1c065a
YaroslavGankov <yaroslav.gankov@transportexchangegroup.com>
2022-10-12 17:50:12 +0300

    removed aws_eip

--- terraform_lock_hcl/layer1.Network/network.tf
+++ terraform_lock_hcl/layer1.Network/network.tf
@@ -79,7 +79,7 @@ resource "aws_route_table" "internal_subnets" {
     vpc_id = aws_vpc.skiff_vpc.id
     route {
         cidr_block = "0.0.0.0/0"
-        nat_gateway_id = aws_nat_gateway.ng.id
+        #nat_gateway_id = aws_nat_gateway.ng.id
     }
     tags = {
         Name = "${var.vpc_cidr_mask16}.x.x-${var.name_project}-internal-${var.env}"
