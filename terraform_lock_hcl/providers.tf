f5c6b8d763f2d72b4f10c5d033c34ce819c2d8ff
YaroslavGankov <yaroslav.gankov@transportexchangegroup.com>
2022-10-12 14:00:53 +0300

    init without lock-files and any backups

---
+++ terraform_lock_hcl/providers.tf
@@ -0,0 +1,19 @@
+#if this file was changed then 'terraform init' will execute....
+#next comment
+#second comment
+#third comment
+#4 comment
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+
+provider "aws" {}
